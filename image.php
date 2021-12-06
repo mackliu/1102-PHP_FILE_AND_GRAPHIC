a0fd7b8211aea136ce66bf142165f7f6ba7a2ae0
mackliu <macklun@gmail.com>
2021-12-06 15:40:19 +0800

    使用表單參數來改變縮放圖片

--- image.php
+++ image.php
@@ -12,6 +12,7 @@
 
 if(isset($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'] , 'img/'.$_FILES['img']['name']);
+    
     switch($_FILES['img']['type']){
         case "image/jpeg":
             $srcimg=imagecreatefromjpeg('img/'.$_FILES['img']['name']);
@@ -27,15 +28,33 @@ if(isset($_FILES['img']['tmp_name'])){
         break;
     }
 
+    $info=getimagesize('img/'.$_FILES['img']['name']);
+    $scaleRate=$_POST['rate'];
+    
+    //目標檔案大小
+    $dwidth=$info[0]*$scaleRate;
+    $dheight=$info[1]*$scaleRate;
 
-    $dstimg=imagecreatetruecolor(240,180);
-    $white=imagecolorallocate($dstimg,255,255,255);
+    if(isset($_POST['border'])){
+        $border=ceil(($dwidth*0.1)/2);
+        
+    }else{
+        $border=0;
+
+    }
+    
+    //內嵌圖片大小
+    $inner_w=$dwidth-($border*2);
+    $inner_h=$dheight-($border*2);
+
+    $dstimg=imagecreatetruecolor($dwidth,$dheight);
+    $white=imagecolorallocate($dstimg,200,200,150);
     imagefill($dstimg,0,0,$white);
     /* imagecopyresampled($dstimg,$srcimg,0,0,0,0,240,180,799,532);
     $filename='img/'.explode(".",$_FILES['img']['name'])[0]."_small.png";
     imagepng($dstimg,$filename); */
    
-    imagecopyresampled($dstimg,$srcimg,20,23,0,0,200,133,799,532);
+    imagecopyresampled($dstimg,$srcimg,$border,$border,0,0,$inner_w,$inner_h,$info[0],$info[1]);
     $filename='img/'.explode(".",$_FILES['img']['name'])[0]."_border.png";
     imagepng($dstimg,$filename);
 
@@ -57,6 +76,12 @@ if(isset($_FILES['img']['tmp_name'])){
 <h1 class="header">圖形處理練習</h1>
 <!---建立檔案上傳機制--->
 <form action="?" method="post" enctype="multipart/form-data">
+    <input type="checkbox" name="border" value="1">是否有邊框<br>
+    <select name="rate" >
+        <option value="0.25">縮小四分之一</option>
+        <option value="0.5">縮小一半</option>
+        <option value="2">放大2倍</option>
+    </select>
      <p><input type="file" name="img" ></p>
      <p><input type="submit" value="上傳"></p>
 </form>
