30d03d1a75bfc103f583b78394d4f9f8d6cf7c7a
mackliu <macklun@gmail.com>
2021-12-06 14:35:12 +0800

    圖形的縮放與上色

--- image.php
+++ image.php
@@ -10,6 +10,39 @@
  * 5.輸出檔案
  */
 
+if(isset($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'] , 'img/'.$_FILES['img']['name']);
+    switch($_FILES['img']['type']){
+        case "image/jpeg":
+            $srcimg=imagecreatefromjpeg('img/'.$_FILES['img']['name']);
+        break;
+        case "image/png":
+            $srcimg=imagecreatefrompng('img/'.$_FILES['img']['name']);
+        break;
+        case "image/gif":
+            $srcimg=imagecreatefromgif('img/'.$_FILES['img']['name']);
+        break;
+        case "image/bmp":
+            $srcimg=imagecreatefrombmp('img/'.$_FILES['img']['name']);
+        break;
+    }
+
+
+    $dstimg=imagecreatetruecolor(240,180);
+    $white=imagecolorallocate($dstimg,255,255,255);
+    imagefill($dstimg,0,0,$white);
+    /* imagecopyresampled($dstimg,$srcimg,0,0,0,0,240,180,799,532);
+    $filename='img/'.explode(".",$_FILES['img']['name'])[0]."_small.png";
+    imagepng($dstimg,$filename); */
+   
+    imagecopyresampled($dstimg,$srcimg,20,23,0,0,200,133,799,532);
+    $filename='img/'.explode(".",$_FILES['img']['name'])[0]."_border.png";
+    imagepng($dstimg,$filename);
+
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,11 +56,25 @@
 <body>
 <h1 class="header">圖形處理練習</h1>
 <!---建立檔案上傳機制--->
+<form action="?" method="post" enctype="multipart/form-data">
+     <p><input type="file" name="img" ></p>
+     <p><input type="submit" value="上傳"></p>
+</form>
 
 
 
 <!----縮放圖形----->
+<?php 
+if(isset($_FILES['img']['name'])){
+ ?>
+ <div>你上傳的圖片為:</div>
+ <img src='img/<?=$_FILES['img']['name'];?>'>
+ <div>縮放後成為:</div>
+ <img src='<?=$filename;?>'>
 
+ <?php
+}
+?>
 
 <!----圖形加邊框----->
 
