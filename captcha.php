e48ab9cf649d5fcddd0858a6c33afaf87d2d52b0
mackliu <macklun@gmail.comm>
2021-12-10 22:26:24 +0800

    增加程式碼註解

--- captcha.php
+++ captcha.php
@@ -35,14 +35,17 @@ $length=rand(4,8);
 }
  echo $str;
 
+ //指定一個圖形大小
  $dstimg=imagecreatetruecolor(200,50);
  $white=imagecolorallocate($dstimg,200,200,180);
  $black=imagecolorallocate($dstimg,0,0,0);
  imagefill($dstimg,0,0,$white);
 
- 
+ //使用迴圈的方把字串中的每一個字元獨立寫入圖形資源中
  for($i=0;$i<$length;$i++){
      $c=mb_substr($str,$i,1);
+     
+     //使用GD內建的字形來寫入圖形資源,字形最大到5點，寫入坐標使用亂數方式來產生
      imagestring($dstimg,5,(10+$i*rand(15,20)),(10+rand(0,10)),$c,$black);
 
  }
